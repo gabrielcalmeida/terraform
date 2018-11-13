03f7f109ef8c0c7b891e5f11d3f074ff35f5ebd1
Gabriel Couto de Almeida <gcalmeida@uolinc.com>
2018-11-13 11:09:30 -0200

    ch02

---
+++ ch02/main.tf
@@ -0,0 +1,48 @@
+# Setando o provider como AWS e regiao
+provider "aws" {
+  region = "us-east-1"
+}
+# Criacao de uma instancia micro
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = "${var.server_port}"
+    to_port     = "${var.server_port}" 
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+# setando o launch conf
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-40d28157"
+  instance_type   = "t2.micro"
+  security_groups = ["${aws_security_group.instance.id}"]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+# ASG
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
